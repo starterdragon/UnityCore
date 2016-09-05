ddf47266234f580bdab0e93e55d13a8e862e3f00
xBeeegsone <begsOne38@gmail.com>
2016-09-05 22:44:27 +0300

    Update CraftingEventPacket.php
    0.15.7 protocol update

--- src/pocketmine/network/protocol/CraftingEventPacket.php
+++ src/pocketmine/network/protocol/CraftingEventPacket.php
@@ -23,66 +23,40 @@ namespace pocketmine\network\protocol;
 
 #include <rules/DataPacket.h>
 
-#ifndef COMPILE
 
-#endif
+class CraftingEventPacket extends DataPacket{
+	const NETWORK_ID = Info::CRAFTING_EVENT_PACKET;
 
-
-use pocketmine\utils\BinaryStream;
-use pocketmine\utils\Utils;
-
-
-abstract class DataPacket extends BinaryStream{
-
-	const NETWORK_ID = 0;
-
-	public $isEncoded = false;
-	private $channel = 0;
-
-	public function pid(){
-		return $this::NETWORK_ID;
-	}
-
-	abstract public function encode();
-
-	abstract public function decode();
-
-	public function reset(){
-		$this->buffer = chr($this::NETWORK_ID);
-		$this->offset = 0;
-	}
-
-	/**
-	 * @deprecated This adds extra overhead on the network, so its usage is now discouraged. It was a test for the viability of this.
-	 */
-	public function setChannel($channel){
-		$this->channel = (int) $channel;
-		return $this;
-	}
-
-	public function getChannel(){
-		return $this->channel;
-	}
+	public $windowId;
+	public $type;
+	public $id;
+	public $input = [];
+	public $output = [];
 
 	public function clean(){
-		$this->buffer = null;
-		$this->isEncoded = false;
-		$this->offset = 0;
-		return $this;
+		$this->input = [];
+		$this->output = [];
+		return parent::clean();
 	}
 
-	public function __debugInfo(){
-		$data = [];
-		foreach($this as $k => $v){
-			if($k === "buffer"){
-				$data[$k] = bin2hex($v);
-			}elseif(is_string($v) or (is_object($v) and method_exists($v, "__toString"))){
-				$data[$k] = Utils::printable((string) $v);
-			}else{
-				$data[$k] = $v;
-			}
+	public function decode(){
+		$this->windowId = $this->getByte();
+		$this->type = $this->getInt();
+		$this->id = $this->getUUID();
+
+		$size = $this->getInt();
+		for($i = 0; $i < $size and $i < 128; ++$i){
+			$this->input[] = $this->getSlot();
 		}
 
-		return $data;
+		$size = $this->getInt();
+		for($i = 0; $i < $size and $i < 128; ++$i){
+			$this->output[] = $this->getSlot();
+		}
 	}
+
+	public function encode(){
+
+	}
+
 }
